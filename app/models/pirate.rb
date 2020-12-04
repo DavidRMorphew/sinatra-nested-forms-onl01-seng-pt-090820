1894fa5c863eb08f84986dc7554bbf7258e2b933
DavidRMorphew <morphewd@umich.edu>
2020-12-03 21:58:46 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_reader :name, :weight, :height
+
+    @@all = []
+
+    def initialize(options)
+        @name = options[:name]
+        @weight = options[:weight]
+        @height = options[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
